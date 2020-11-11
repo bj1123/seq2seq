72e7aec9203bb3716646fb635d40f2e99ac2f482
bj1123 <bj1123@daum.net>
2020-11-11 09:01:51 +0900

    v0.2

--- util/initializer.py
+++ util/initializer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Initializer:
     def __init__(self,mode:str,value:float,dropout:float):
         self.mode = mode
@@ -30,39 +31,21 @@ class Initializer:
             if hasattr(m, 'vec_v'):
                 self.init_weight(m.vec_v)
 
-        elif classname.find('StaticBoundaryDecision') != -1:
-            if hasattr(m, 'q'):
-                self.init_weight(m.q)
-
-        elif classname.find('Factorized_SoftmaxV2') != -1:
-            if hasattr(m, 'logits'):
-                self.init_weight(m.logits)
-
-        elif classname.find('Transformer_Model') != -1:
+        elif classname.find('TransformerModel') != -1:
             if hasattr(m, 'rw_bias'):
                 self.init_weight(m.rw_bias)
             if hasattr(m, 'rr_bias'):
                 self.init_weight(m.rr_bias)
 
-        elif classname.find('Two_Stream_Multihead_Att') != -1:
-            if hasattr(m, 'vec_u'):
-                self.init_weight(m.vec_u)
-            if hasattr(m, 'vec_v'):
-                self.init_weight(m.vec_v)
-
-        elif classname.find('XLNet') != -1:
-            if hasattr(m, 'vec_g'):
-                self.init_weight(m.vec_g)
-
         elif classname.find('Embedding') != -1:
             if hasattr(m, 'weight'):
                 self.init_weight(m.weight)
-        #
-        # elif classname.find('LayerNorm') != -1:
-        #     if hasattr(m, 'weight'):
-        #         nn.init.normal_(m.weight, 1.0, self.value)
-        #     if hasattr(m, 'bias') and m.bias is not None:
-        #         self.init_bias(m.bias)
+
+        elif classname.find('LayerNorm') != -1:
+            if hasattr(m, 'weight'):
+                nn.init.normal_(m.weight, 1.0, self.value)
+            if hasattr(m, 'bias') and m.bias is not None:
+                self.init_bias(m.bias)
 
     def initialize(self, m):
         m.apply(self.case_initialize)
