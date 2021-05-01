adaptive softmax bugfix

model/embeddings.py
@@ -104,8 +104,8 @@ class AdaptiveBase(nn.Module):
         super(AdaptiveBase, self).__init__()
         self.vocab_size = vocab_size
         self.n_cluster = len(cutoffs) + 1 if cutoffs is not None else 4
-        self.projection_dim = projection_dim
-        self.scale = projection_dim ** 0.5
+        self.projection_dim = hidden_dim
+        self.scale = hidden_dim ** 0.5
         cutoffs = cutoffs if cutoffs else self.compute_cutoffs()
         self.cutoffs = [0] + cutoffs + [vocab_size]
         self.embedding_dims = [hidden_dim // (div_val ** i) for i in range(self.n_cluster)]
@@ -209,13 +209,13 @@ class TransformerEmbedding(nn.Module):
 
         # self.word_embedding = HybridEmbedding(vocab_size, embedding_dim, padding_index, dropout_rate)
         # self.word_embedding = StructuredEmbedding(vocab_size, embedding_dim)
-        self.word_embedding = AdaptiveEmbedding(vocab_size, embedding_dim, embedding_dim)
+        # self.word_embedding = AdaptiveEmbedding(vocab_size, embedding_dim, embedding_dim)
         # self.word_embedding = OneEmbed(vocab_size, embedding_dim, padding_index,
         #                                one_emb_type='real', dropout=dropout_rate)
         # self.word_embedding = HashEmbedding(vocab_size, embedding_dim, padding_index)
-        # self.word_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_index)
+        self.word_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_index)
 
-        self.word_ln = nn.LayerNorm(embedding_dim)
+        # self.word_ln = nn.LayerNorm(embedding_dim, elementwise_affine=False)
         # self.scale = nn.Parameter(torch.Tensor([0.02]))
         # self.pos_ln = nn.LayerNorm(embedding_dim)
         if self.use_pos_emb:
@@ -228,7 +228,7 @@ class TransformerEmbedding(nn.Module):
         ks = qs + ms
         emb = self.word_embedding(x)
         # emb = self.word_ln(emb) * self.scale
-        emb = self.word_ln(emb) * self.posisition_embedding.weight.std(dim=0).detach()
+        # emb = self.word_ln(emb) * self.posisition_embedding.weight.std(dim=0).detach()
         if self.use_pos_emb:
             emb *= math.sqrt(self.embedding_dim)
             pos_indicator = torch.arange(ms, ks, 1).clamp_max_(self.seq_len).to(emb.device)

model/softmax.py
@@ -13,19 +13,19 @@ class SemiAdaptiveSoftmax(AdaptiveBase):
         self.projections = nn.ModuleList()
         self.tails = nn.ModuleList()
 
-        for i in range(1, self.n_clusters):
-            n_vocabs = self.cutoffs[i] + self.cutoffs[i+1]
+        for i in range(1, self.n_cluster):
+            n_vocabs = self.cutoffs[i+1] - self.cutoffs[i]
             self.projections.append(nn.Linear(hidden_dim, self.embedding_dims[i], bias=False))
-            self.tails.append(nn.Linear(self.proj_dims[i], n_vocabs, bias=False))
+            self.tails.append(nn.Linear(self.embedding_dims[i], n_vocabs, bias=False))
 
-    def call(self, x):
+    def forward(self, x):
         head_word_logits = self.head(x)
         tail_word_logits = []
-        for i in range(self.n_clusters-1):
+        for i in range(self.n_cluster-1):
             proj = self.projections[i](x)
             tail = self.tails[i](proj)
             tail_word_logits.append(tail)
-        return tf.concat([head_word_logits, *tails_sum_logits], -1)
+        return torch.cat([head_word_logits, *tail_word_logits], -1)
 
 
 class AdaptiveSoftmax(nn.Module):

model/transformer.py
@@ -341,7 +341,7 @@ class EncoderDecoderModel(EncoderDecoderBase):
             self.final = lambda x: torch.matmul(x, embedding_weight.T)
 
         else:
-            self.final = SemiAdaptiveSoftmax(hidden_dim, vocab_size)
+            self.final = SemiAdaptiveSoftmax(vocab_size, hidden_dim)
             # self.final = nn.Linear(hidden_dim, vocab_size, bias=False)
 
     def encode_src(self, inp):